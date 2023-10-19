2a2c90969a8924e1fde81e0101737443c9981780
Melfu <degardenzi.clement@gmail.com>
2023-10-19 16:02:54 +0200

    fonction des click sur les filtres

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -6,10 +6,12 @@ fetch("http://localhost:5678/api/works")
 })
 //Génération du contenu de la page
 .then(result => {
+    
     const gallery = document.querySelector(".gallery");
     const allWorksList = result;
     console.log(allWorksList);
     function OnClickfilterAll(){
+        gallery.innerHTML = " ";
         for(let i = 0; i < allWorksList.length; i++){
             let fig = document.createElement("figure");
             let figImg = document.createElement("img");
@@ -20,7 +22,17 @@ fetch("http://localhost:5678/api/works")
             fig.append(figImg, figCaption);
             gallery.appendChild(fig);
         };
+        console.log("click");
     }
+    //Création du filtres "Tous"
+    let filterBar = document.querySelector(".filters");
+    let filterBtnAll = document.createElement("button");
+    filterBtnAll.innerText = "Tous";
+    filterBtnAll.classList.add("filter-btn", "filter-btn-active");
+    filterBtnAll.addEventListener("click", () =>{
+        OnClickfilterAll()
+    });
+    filterBar.appendChild(filterBtnAll);
     OnClickfilterAll();
     return allWorksList;
 })
@@ -32,26 +44,42 @@ fetch("http://localhost:5678/api/works")
     console.log(filteredCategoriesList);
     
     //Création des boutons de filtres
-    const filterBar = document.querySelector(".filters");
-    let filterBtnAll = document.createElement("button");
-    filterBtnAll.innerHTML = "Tous";
-    filterBtnAll.classList.add("filter-btn", "filter-btn-active");
-    filterBar.appendChild(filterBtnAll);
+    
 
+    function OnClickfilter(categorie){
+        const gallery = document.querySelector(".gallery");
+        if(categorie === "all"){
+            // afficher Tout
+            OnClickfilterAll();
+        }else{
+            gallery.innerHTML = "";
+            const filteredWorks = allWorksList.filter(function (allWorksList){
+                return allWorksList.category.name === categorie
+            });
+            for(let i = 0; i < filteredWorks.length; i++){
+                let fig = document.createElement("figure");
+                let figImg = document.createElement("img");
+                figImg.src = filteredWorks[i].imageUrl;
+                figImg.alt = filteredWorks[i].title;
+                let figCaption = document.createElement("figcaption");
+                figCaption.innerHTML = filteredWorks[i].title;
+                fig.append(figImg, figCaption);
+                gallery.appendChild(fig);
+            };
+        }
+    }
+
+    let filterBar = document.querySelector(".filters");
     filteredCategoriesList.forEach((categorie) => {
         let filterBtn = document.createElement("button");
         filterBtn.classList.add("filter-btn");
         filterBtn.innerText = categorie;
-        filterBtn.data;
+        filterBtn.addEventListener("click", () => {
+            OnClickfilter(categorie);
+        });
         filterBar.appendChild(filterBtn)
     })
-    // function OnClickfilter(categorie){
-    //     gallery.innerHTML = "";
-    //     if(categorie === undefined){
-    //         // afficher Tout
-    //         OnClickfilterAll();
-    //     }
-    // }
+    
 
 })
 
