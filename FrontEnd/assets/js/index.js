dc00a370ce93aaa1dee901b4aaf48f02e90e91fc
Melfu <degardenzi.clement@gmail.com>
2023-10-31 16:51:45 +0100

    modale ok POST à suivre

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,17 +1,23 @@
 
 const gallery = document.querySelector(".gallery");
 let filterBar = document.querySelector(".filters");
-let modal = document.querySelector(".modal")
+let modal = document.querySelector(".modal");
+let modalWrapperEdit = document.querySelector(".modal-wrapper-edit");
+let modalWrapperForm = document.querySelector(".modal-wrapper-form");
+
 function stopPropagation(e) {
     e.stopPropagation
-}
-function openModal(modale) {
+};
+function openModal(e) {
     modal.style.display = null;
-    modale.style.display = null
-}
+    modalWrapperEdit.style.display = null;
+};
 function closeModal(e) {
-    modal.style.display = "none"
-}
+    modal.style.display = "none";
+    modalWrapperEdit.style.display = "none";
+    modalWrapperForm.style.display = "none";
+
+};
 
 //Récupération des données de l'API
 fetch("http://localhost:5678/api/works")
@@ -51,7 +57,7 @@ fetch("http://localhost:5678/api/works")
 
         //Récupération des Catégories
         let setCategoriesList = new Set(allWorksList.map(allWorksList => allWorksList.category.name));
-
+        console.log(setCategoriesList);
         // Vérification du token
         if (window.localStorage.getItem("token") != undefined) {
             // Modification du bouton Login en Logout
@@ -69,22 +75,10 @@ fetch("http://localhost:5678/api/works")
             const portfolioHeader = document.querySelector(".portfolio__header");
             portfolioHeader.appendChild(modifyWorksBtn)
 
-            //Ouverture et Fermeture de la modale
-            let modalWrapperEdit = document.querySelector(".modal-wrapper-edit");
-            let modalWrapperForm = document.querySelector(".modal-wrapper-form");
+            //Ouverture  de la modale
             modifyWorksBtn.addEventListener("click", () => {
                 openModal(modalWrapperEdit)
             });
-
-            const exitCross = document.querySelector(".exit-cross");
-            const exitModal = document.querySelector(".modal-bg");
-            exitCross.addEventListener("click", () => {
-                closeModal()
-            });
-            exitCross.addEventListener("click", () => {
-                closeModal()
-            });
-            exitModal.addEventListener("click", stopPropagation);
 
             //Contenu de la modale
             let galleryContent = allWorksList.map(allWorksList => allWorksList.imageUrl);
@@ -100,19 +94,22 @@ fetch("http://localhost:5678/api/works")
                 editWork.append(editImg, deleteBtn);
                 galleryEdit.appendChild(editWork)
             });
+
             // Bouton d'envoi vers le formulaire d'ajout d'image
             let addWorkBtn = document.querySelector(".add-work-btn");
             addWorkBtn.addEventListener("click", () => {
                 modalWrapperEdit.style.display = "none";
                 modalWrapperForm.style.display = null;
             });
+
             // Flèche de retour à la gallerie d'édition
             let leftArrow = document.querySelector(".arrow-left");
             leftArrow.addEventListener("click", () => {
                 modalWrapperForm.style.display = "none";
                 modalWrapperEdit.style.display = null;
             });
-            // Fonction du formulaire
+
+            //Catégories du formulaire
             let categorySelection = document.querySelector("#form-category");
             setCategoriesList.forEach((categorie) => {
                 let option = document.createElement("option");
@@ -120,29 +117,17 @@ fetch("http://localhost:5678/api/works")
                 option.innerText = categorie;
                 categorySelection.appendChild(option)
             });
-            // const submitBtn = document.querySelector(".modal-wrapper-form button");
-            // const workForm = document.querySelector(".modal-wrapper-form form");
-            const pictureTitle = document.getElementById("picture-title").value;
-            // const category = document.getElementById("form-category").value;
-            const pictureFile = document.getElementById("picture");
-            const preview = document.getElementById("preview");
-            const imagePlaceholder = document.getElementById("img-placeholder");
-            const pictureAddBlock = document.querySelector(".picture-add-block");
-            pictureFile.addEventListener("change", function() {
-                if (pictureFile.files && pictureFile.files[0]) {
-                    
-                    const reader = new FileReader();
-    
-                    reader.onload = function(e) {
-                        preview.src = e.target.result;
-                        preview.style.display = null;
-                    };
-                    pictureAddBlock.removeChild(imagePlaceholder);
-                    reader.readAsDataURL(pictureFile.files[0]);
-                }
+
+            // Fermeture de la modale
+            const exitCross = document.querySelector(".exit-cross");
+            const exitModal = document.querySelector(".modal-bg");
+            exitCross.addEventListener("click", () => {
+                closeModal()
             });
-
-
+            exitModal.addEventListener("click", () => {
+                closeModal()
+            });
+            exitModal.addEventListener("click", stopPropagation);
 
         } else {
             //Création du bouton filtre "Tous"
@@ -191,3 +176,45 @@ fetch("http://localhost:5678/api/works")
     });
 
 
+
+// Preview de l'image Uploadée
+const filePicture = document.getElementById("picture");
+const filePreview = document.getElementById("preview");
+const fileInput = document.querySelector(".file-input")
+const submitBtn = document.querySelector(".modal-wrapper-form .modal-btn");
+const filePictureReader = new FileReader();
+
+filePicture.addEventListener("change", function () {
+    if (filePicture.files && filePicture.files[0]) {
+        filePictureReader.onload = function (e) {
+            filePreview.src = e.target.result;
+            filePreview.style.display = null;
+        };
+        fileInput.style.display = "none"
+        submitBtn.classList.remove("disabled")
+        filePictureReader.readAsDataURL(filePicture.files[0]);
+        filePictureReader.onloadend = console.log(filePictureReader)
+    };
+});
+
+// Récupération des données du formulaire
+const filePictureTitle = document.getElementById("picture-title");
+const fileCategory = document.getElementById("form-category");
+const addWorkForm = document.getElementById("add-work-form");
+
+addWorkForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    //Récupérer l'ID de catégorie
+    //pour chaque valeur de setCategoriesList vérifier si elle correspond
+    //à la valeur de fileCategory et renvoyer la valeur d'index
+
+    // Créer l'objet d'envoi POST
+    // let workToPost = {
+    //     "id": 0,
+    //     "title": filePictureTitle.value,
+    //     "imageUrl": "string"
+    //     "categoryId": "string",
+    //     "userId": 0
+    // }
+    console.log(workToPost)
+});
